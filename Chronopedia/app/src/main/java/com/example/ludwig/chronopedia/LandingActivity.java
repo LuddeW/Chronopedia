fdaf36bbc5194374b41ab97d803a33ed12a44ba3
LuddeW <ludwig.wiktorsson@gmail.com>
2017-10-24 13:52:45 +0200

    testcomitt

--- Chronopedia/app/src/main/java/com/example/ludwig/chronopedia/LandingActivity.java
+++ Chronopedia/app/src/main/java/com/example/ludwig/chronopedia/LandingActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LandingActivity extends AppCompatActivity {
-    public int testInt = 0;
 
 
     @Override
