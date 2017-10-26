a6ef4bb95b7518919b7a683ae8e369b3899183e0
LuddeW <ludwig.wiktorsson@gmail.com>
2017-10-26 14:30:11 +0200

    Prepaird so i can se if i get the right response from the JSON on the landingactivity :+1:

--- Chronopedia/app/src/main/java/Managers/ApiManager.java
+++ Chronopedia/app/src/main/java/Managers/ApiManager.java
@@ -1,15 +1,15 @@
 package Managers;
 
+import android.widget.TextView;
+
+import com.example.ludwig.chronopedia.R;
+
 /**
  * Created by Ludwig on 2017-10-24.
  */
 
 public class ApiManager {
     // hämtar apier från Wiki och Darksky
-
-    public ApiManager(){
-
-    }
 
     public String returnWeather(){
         return("Sun");
@@ -18,4 +18,5 @@ public class ApiManager {
     public String returnWikiQuote(){
         return ("2003 – Concorde makes its last commercial flight.");
     }
+
 }
--- Chronopedia/app/src/main/java/com/example/ludwig/chronopedia/LandingActivity.java
+++ Chronopedia/app/src/main/java/com/example/ludwig/chronopedia/LandingActivity.java
@@ -2,12 +2,24 @@ package com.example.ludwig.chronopedia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import Managers.ApiManager;
 
 public class LandingActivity extends AppCompatActivity {
-
+    TextView landingTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        initComponents();
+    }
+
+    private void initComponents(){
+        landingTextView = (TextView) findViewById(R.id.landingTextView);
+        ApiManager apiManager = new ApiManager();
+        Log.d("apimanager", apiManager.returnWeather());
+        landingTextView.setText(apiManager.returnWeather());
     }
 }
