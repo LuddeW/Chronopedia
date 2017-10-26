55c0bcd46f1c20805c2e50e5864576c3ea0640d5
LuddeW <ludwig.wiktorsson@gmail.com>
2017-10-26 14:43:41 +0200

    Trying to get a response from googles API to start with :rocket:

--- Chronopedia/app/src/main/java/Managers/ApiManager.java
+++ Chronopedia/app/src/main/java/Managers/ApiManager.java
@@ -1,7 +1,14 @@
 package Managers;
 
+import android.content.Context;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.ludwig.chronopedia.R;
 
 /**
@@ -12,8 +19,23 @@ public class ApiManager {
     // hämtar apier från Wiki och Darksky
     String returnWeatherString = "Sun";
     String returnWikiString = "2003 – Concorde makes its last commercial flight." ;
-    public String returnWeather(){
+    //RequestQueue queue = Volley.newRequestQueue(//Some kind of context here);
+    String url ="http://www.google.com";
 
+    public String returnWeather(){
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        returnWeatherString = "Response is: "+ response.substring(0,500);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                returnWeatherString = "That didn't work!";
+            }
+        });
         return(returnWeatherString);
     }
 
