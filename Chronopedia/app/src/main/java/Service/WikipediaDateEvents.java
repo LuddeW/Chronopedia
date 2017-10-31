bdf4ff140def96a4063b180d464ea1d3c5cd3fc4
LuddeW <ludwig.wiktorsson@gmail.com>
2017-10-31 12:52:12 +0100

    Added a random event from current date

--- Chronopedia/app/src/main/java/Service/WikipediaDateEvents.java
+++ Chronopedia/app/src/main/java/Service/WikipediaDateEvents.java
@@ -1,9 +1,55 @@
 package Service;
 
+import android.os.AsyncTask;
+import android.provider.DocumentsContract;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Created by Ludwig on 2017-10-31.
  */
 
-public class WikipediaDateEvents {
+public class WikipediaDateEvents extends AsyncTask<Void, Void, Void> {
+    String event;
+    TextView wikiTextView;
 
+    public WikipediaDateEvents(TextView wikiTextView){
+        this.wikiTextView = wikiTextView;
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+
+        try {
+            Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/October_31").get();
+            ArrayList<String> events = new ArrayList<>();
+            Elements lists = doc.select("li");
+            for (Element list : lists ){
+                events.add(list.text());
+            }
+            event = events.get(randomInt());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        wikiTextView.setText(event);
+    }
+
+    private int randomInt(){
+        int randomNum = ThreadLocalRandom.current().nextInt(20, 45 + 1);
+        return randomNum;
+    }
 }
--- Chronopedia/app/src/main/java/com/example/ludwig/chronopedia/LandingActivity.java
+++ Chronopedia/app/src/main/java/com/example/ludwig/chronopedia/LandingActivity.java
@@ -3,6 +3,7 @@ package com.example.ludwig.chronopedia;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,17 +11,18 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import Data.Channel;
 import Data.Item;
 import Models.Alarm;
 import Service.WeatherServiceCallback;
+import Service.WikipediaDateEvents;
 import Service.YahooWeatherService;
 
 public class LandingActivity extends AppCompatActivity implements WeatherServiceCallback {
     TextView conditionTextView;
     TextView temperatureTextView;
     TextView locationTextView;
+    TextView wikiTextView;
     ImageView weatherIconImageView;
     Button landingButton;
     DatabaseHandler handler = new DatabaseHandler(this);
@@ -39,6 +41,7 @@ public class LandingActivity extends AppCompatActivity implements WeatherService
         conditionTextView = (TextView) findViewById(R.id.conditionTextView);
         temperatureTextView = (TextView) findViewById(R.id.temperatureTextView);
         locationTextView = (TextView) findViewById(R.id.locationTextView);
+        wikiTextView = (TextView) findViewById(R.id.wikiTextView);
         weatherIconImageView = (ImageView) findViewById(R.id.weatherIconImageView);
         landingButton = (Button) findViewById(R.id.landingButton);
 
@@ -47,6 +50,7 @@ public class LandingActivity extends AppCompatActivity implements WeatherService
         dialog.setMessage("Loading...");
         dialog.show();
         service.refreshWeather("Malmo, Sweden");
+        new WikipediaDateEvents(wikiTextView).execute();
 
         landingButton.setOnClickListener(new View.OnClickListener() {
             @Override
