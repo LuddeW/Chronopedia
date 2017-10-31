4cce6394fd28400c89ae6e7dd74f9cc8d82cd451
LuddeW <ludwig.wiktorsson@gmail.com>
2017-10-31 13:42:41 +0100

    Added so the date is not "Hardcoded" in the URL from Wikipedia

--- Chronopedia/app/src/main/java/Service/WikipediaDateEvents.java
+++ Chronopedia/app/src/main/java/Service/WikipediaDateEvents.java
@@ -11,6 +11,9 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
@@ -29,7 +32,7 @@ public class WikipediaDateEvents extends AsyncTask<Void, Void, Void> {
     protected Void doInBackground(Void... voids) {
 
         try {
-            Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/October_31").get();
+            Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/" + getMonth()+ "_" + getDate()).get();
             ArrayList<String> events = new ArrayList<>();
             Elements lists = doc.select("li");
             for (Element list : lists ){
@@ -51,5 +54,17 @@ public class WikipediaDateEvents extends AsyncTask<Void, Void, Void> {
     private int randomInt(){
         int randomNum = ThreadLocalRandom.current().nextInt(20, 45 + 1);
         return randomNum;
+    }
+
+    private String getMonth(){
+        Calendar mCalendar = Calendar.getInstance();
+        String month = mCalendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
+        return month;
+    }
+
+    private String getDate(){
+        Date d = new Date();
+        String dateWithoutTime = d.toString().substring(8, 10);
+        return dateWithoutTime;
     }
 }
